119fe45af323d5fb5a2b6687763e9cfe983a5da0
jaahaan <cse_1912020239@lus.ac.bd>
2021-10-17 21:03:51 +0600

    University App

--- app/src/main/java/com/example/luhelpmate/CourseOfferingsActivity.java
+++ app/src/main/java/com/example/luhelpmate/CourseOfferingsActivity.java
@@ -25,42 +25,45 @@ public class CourseOfferingsActivity extends AppCompatActivity implements View.O
 
     @Override
     public void onClick(View v) {
-        //For user input
-        EditText editText = findViewById(R.id.batchC);
-        String name = editText.getText().toString();
-        int i = Integer.parseInt(name);
 
         TextView headerC = findViewById(R.id.headerC);
         String f = "Fall-2021";
         ArrayList<Object> objects = new ArrayList<>();
+        try {
+            //For user input
+            EditText editText = findViewById(R.id.batchC);
+            String name = editText.getText().toString();
+            int i = Integer.parseInt(name);
 
+            if (i == 58) {
+                String s = f + "\n1st Semester, Batch : " + i;
+                headerC.setText(s);
+                objects.add(new Object("Course Code", "Course Title", "Credit", "Prerequisite"));
+                objects.add(new Object("ENG-1111", "English Reading and Speaking", "3", "-"));
+                objects.add(new Object("MAT-1111", "Differential and Integral Calculus", "3", "-"));
+                objects.add(new Object("CSE-1111", "Introduction To Computers", "2", "-"));
+                objects.add(new Object("CSE-1112", "Introduction To Computers Sessional", "1", "-"));
+                objects.add(new Object("ART-1111", "Bangladesh Studies", "3", "-"));
+                objects.add(new Object("CEE-2110", "Engineering Drawing", "1.5", "-"));
+                objects.add(new Object("", "Total Credit", "13.5", ""));
+            } else if (i == 57) {
+                String s = f + "\n2nd Semester, Batch : " + i;
+                headerC.setText(s);
+                objects.add(new Object("Course Code", "Course Title", "Credit", "Prerequisite"));
+                objects.add(new Object("CSE-1213", "Computer Programming", "3", "-"));
+                objects.add(new Object("CSE-1214", "Computer Programming Sessional", "1", "-"));
+                objects.add(new Object("CSE-1215", "Discrete Mathematics", "3", "-"));
+                objects.add(new Object("ART-1311", "Introduction to Sociology", "3", "-"));
+                objects.add(new Object("MAT-1213", "Linear Algebra & Complex Analysis", "3", "-"));
+                objects.add(new Object("", "Total Credit", "13", ""));
+            } else {
+                headerC.setText("");
+                Toast.makeText(this, getString(R.string.toast1), Toast.LENGTH_SHORT).show();
+            }
+        } catch (NumberFormatException e) {
+            headerC.setText("");
+            Toast.makeText(this, getString(R.string.toast2), Toast.LENGTH_SHORT).show();
 
-        if (i == 58) {
-            String s = f + "\n1st Semester, Batch : " + i;
-            headerC.setText(s);
-            objects.add(new Object("Course Code", "Course Title", "Credit", "Prerequisite"));
-            objects.add(new Object("ENG-1111", "English Reading and Speaking", "3", "-"));
-            objects.add(new Object("MAT-1111", "Differential and Integral Calculus", "3", "-"));
-            objects.add(new Object("CSE-1111", "Introduction To Computers", "2", "-"));
-            objects.add(new Object("CSE-1112", "Introduction To Computers Sessional", "1", "-"));
-            objects.add(new Object("ART-1111", "Bangladesh Studies", "3", "-"));
-            objects.add(new Object("CEE-2110", "Engineering Drawing", "1.5", "-"));
-            objects.add(new Object("", "Total Credit", "13.5", ""));
-        }
-
-        else if (i == 57) {
-            String s = f + "\n2nd Semester, Batch : " + i;
-            headerC.setText(s);
-            objects.add(new Object("Course Code", "Course Title", "Credit", "Prerequisite"));
-            objects.add(new Object("CSE-1213", "Computer Programming", "3", "-"));
-            objects.add(new Object("CSE-1214", "Computer Programming Sessional", "1", "-"));
-            objects.add(new Object("CSE-1215", "Discrete Mathematics", "3", "-"));
-            objects.add(new Object("ART-1311", "Introduction to Sociology", "3", "-"));
-            objects.add(new Object("MAT-1213", "Linear Algebra & Complex Analysis", "3", "-"));
-            objects.add(new Object("", "Total Credit", "13", ""));
-        }
-        else {
-            Toast.makeText(this, getString(R.string.toast1), Toast.LENGTH_SHORT).show();
         }
         ListView course = findViewById(R.id.list);
 
--- app/src/main/java/com/example/luhelpmate/RoutineActivity.java
+++ app/src/main/java/com/example/luhelpmate/RoutineActivity.java
@@ -24,44 +24,63 @@ public class RoutineActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.routine_listview);
         button = findViewById(R.id.fetchR);
         button.setOnClickListener(this);
+        TextView se = findViewById(R.id.session);
+        TextView lu = findViewById(R.id.lastUpdate);
+
+        String session = "Fall-2021";
+        String lastUpdated = "Last Updated: 10.10.2021";
+
+        se.setText(session);
+        lu.setText(lastUpdated);
+
     }
 
     @Override
     public void onClick(View v) {
-        EditText batch = findViewById(R.id.batchR);
-        String name = batch.getText().toString();
-        int i = Integer.parseInt(name);
 
-        EditText section = findViewById(R.id.sectionR);
-        String j = section.getText().toString();
-
-        TextView headerR = findViewById(R.id.headerR);
-
-        String f = "Fall-2021";
         ArrayList<Object> objects = new ArrayList<>();
+        TextView headerR = findViewById(R.id.headerR);
+        try {
+            EditText batch = findViewById(R.id.batchR);
+            String name = batch.getText().toString();
+            int i = Integer.parseInt(name);
 
-        if (i == 58 && (j.equals("A") || j.equals("a"))) {
-            String s = f + "\n1st Semester, Batch : " + i + ", Section: " + j;
-            headerR.setText(s);
+            EditText section = findViewById(R.id.sectionR);
+            String j = section.getText().toString();
+            if (i == 58 && (j.equals("A") || j.equals("a"))) {
+                String s = "1st Semester, Batch : " + i + ", Section: " + j;
+                headerR.setText(s);
 
-            objects.add(new Object("", "Monday", "", ""));
-            objects.add(new Object("11.00 AM-11.50 AM", "EEE-1111", "JTE", "*"));
-            objects.add(new Object("12.00 PM-12.50 PM", "EEE-1111", "JTE", "*"));
-            objects.add(new Object("01.00 PM-01.50 PM", "Break", "", ""));
-            objects.add(new Object("01.00 PM-01.50 PM", "Break", "", ""));
+                objects.add(new Object("", "MONDAY", "", ""));
+                objects.add(new Object("11.00 AM-11.50 AM", "EEE-1111", "JTE", "*"));
+                objects.add(new Object("12.00 PM-12.50 PM", "EEE-1111", "JTE", "*"));
+                objects.add(new Object("01.00 PM-01.50 PM", "Break", "", ""));
 
-            objects.add(new Object("", "Wed", "", ""));
+                objects.add(new Object("", "THURSDAY", "", ""));
+                objects.add(new Object("11.00 AM-11.50 AM", "EEE-1111", "JTE", "*"));
+
+            } else if (i == 57 && j.equals("")) {
+                String s = "2st Semester, Batch : " + i;
+                headerR.setText(s);
+
+                objects.add(new Object("", "MONDAY", "", ""));
+                objects.add(new Object("11.00 AM-11.50 AM", "EEE-1111", "JTE", "*"));
+                objects.add(new Object("12.00 PM-12.50 PM", "EEE-1111", "JTE", "*"));
+                objects.add(new Object("01.00 PM-01.50 PM", "Break", "", ""));
+
+                objects.add(new Object("", "THURSDAY", "", ""));
+                objects.add(new Object("11.00 AM-11.50 AM", "EEE-1111", "JTE", "*"));
+
+
+            } else {
+                headerR.setText("");
+                Toast.makeText(this, getString(R.string.toast1), Toast.LENGTH_SHORT).show();
+            }
+        } catch (NumberFormatException e) {
+            headerR.setText("");
+            Toast.makeText(this, getString(R.string.toast2), Toast.LENGTH_SHORT).show();
+
         }
-        else if (i == 57) {
-            String s = f + "\n2st Semester, Batch : " + i;
-            headerR.setText(s);
-
-            
-        }
-        else {
-            Toast.makeText(this, getString(R.string.toast1), Toast.LENGTH_SHORT).show();
-        }
-
         ListView routine = findViewById(R.id.list);
 
         final RoutineAdapter adapter = new RoutineAdapter(this, objects);
